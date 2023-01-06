82e38d3f7545dcceaa4ddff3d596ada7f110bb1a
Ulysse248 <u.desgroppes@free.fr>
2023-01-06 15:34:49 +0100

    test changes

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -14,3 +14,11 @@ class TestZone:
         assert b == [[" ", " ", " "],
                      [" ", " ", " "],
              [" ", " ", " "]]
+
+def test_announce_win(capsys):
+    symbol = "X"
+    ttt.announce_win(symbol)
+
+    captured = capsys.readouterr()
+
+    assert captured.out ==  "Player X just nicked the game"
