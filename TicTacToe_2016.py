dc3d4f56ef6a775415b24f0d06c9d202c6ce4fb4
Ulysse248 <u.desgroppes@free.fr>
2023-01-06 15:14:33 +0100

    fix issues

--- TicTacToe_2016.py
+++ TicTacToe_2016.py
@@ -1,6 +1,15 @@
 # Pam Qian 2016 Fall CS 112 Python Midterm Project II
 # Tic Tack Toe 
 
+PLAYER = "Player "
+
+PICK_A_COL = "Pick a column: [left column: enter 0, middle column: enter 1, right column enter 2]"
+
+PICK_A_ROW = "Pick a row: [upper row: enter 0, middle row: enter 1, bottom row: enter 2]:"
+
+def announce_win(symbol):
+    print(PLAYER + symbol + " just nicked the game")
+
 def intro():
 # This function introduces the rules of the game Tic Tac Toe
     print("Hello! Welcome to Pam's Tic Tac Toe game!")
@@ -39,7 +48,7 @@ def sym():
 
 
 
-def startGamming(board, symbol_1, symbol_2, count):
+def start_gaming(board, symbol_1, symbol_2, count):
 # This function starts the game.
 
     # Decides the turn
@@ -47,28 +56,22 @@ def startGamming(board, symbol_1, symbol_2, count):
         player = symbol_1
     elif count % 2 == 1:
         player = symbol_2
-    print("Player "+ player + ", it is your turn. ")
-    row = int(input("Pick a row:"
-                    "[upper row: enter 0, middle row: enter 1, bottom row: enter 2]:"))
-    column = int(input("Pick a column:"
-                       "[left column: enter 0, middle column: enter 1, right column enter 2]"))
+    print(PLAYER+ player + ", it is your turn. ")
+    row = int(input(PICK_A_ROW))
+    column = int(input(PICK_A_COL))
 
 
     # Check if players' selection is out of range
     while (row > 2 or row < 0) or (column > 2 or column < 0):
-        outOfBoard(row, column)
-        row = int(input("Pick a row[upper row:"
-                        "[enter 0, middle row: enter 1, bottom row: enter 2]:"))
-        column = int(input("Pick a column:"
-                           "[left column: enter 0, middle column: enter 1, right column enter 2]"))
+        out_of_board(row, column)
+        row = int(input(PICK_A_ROW))
+        column = int(input(PICK_A_COL))
 
         # Check if the square is already filled
     while (board[row][column] == symbol_1)or (board[row][column] == symbol_2):
-        filled = illegal(board, symbol_1, symbol_2, row, column)
-        row = int(input("Pick a row[upper row:"
-                        "[enter 0, middle row: enter 1, bottom row: enter 2]:"))
-        column = int(input("Pick a column:"
-                            "[left column: enter 0, middle column: enter 1, right column enter 2]"))    
+        illegal()
+        row = int(input(PICK_A_ROW))
+        column = int(input(PICK_A_COL))    
         
     # Locates player's symbol on the board
     if player == symbol_1:
@@ -81,13 +84,13 @@ def startGamming(board, symbol_1, symbol_2, count):
 
 
 
-def isFull(board, symbol_1, symbol_2):
+def is_full(board, symbol_1, symbol_2):
     count = 1
     winner = True
 # This function check if the board is full
     while count < 10 and winner == True:
-        gaming = startGamming(board, symbol_1, symbol_2, count)
-        pretty = printPretty(board)
+        start_gaming(board, symbol_1, symbol_2, count)
+        print_pretty(board)
         
         if count == 9:
             print("The board is full. Game over.")
@@ -95,7 +98,7 @@ def isFull(board, symbol_1, symbol_2):
                 print("There is a tie. ")
 
         # Check if here is a winner
-        winner = isWinner(board, symbol_1, symbol_2, count)
+        winner = is_winner(board, symbol_1, symbol_2)
         count += 1
     if winner == False:
         print("Game over.")
@@ -105,16 +108,15 @@ def isFull(board, symbol_1, symbol_2):
 
 
 
-def outOfBoard(row, column):
+def out_of_board():
 # This function tells the players that their selection is out of range
-    print("Out of boarder. Pick another one. ")
+    print("Out of board. Pick another one. ")
     
     
 
-def printPretty(board):
+def print_pretty(board):
 # This function prints the board nice!
     rows = len(board)
-    cols = len(board)
     print("---+---+---")
     for r in range(rows):
         print(board[r][0], " |", board[r][1], "|", board[r][2])
@@ -123,51 +125,43 @@ def printPretty(board):
 
 
 
-def isWinner(board, symbol_1, symbol_2, count):
+def is_winner(board, symbol_1, symbol_2):
 # This function checks if any winner is winning
     winner = True
     # Check the rows
     for row in range (0, 3):
         if (board[row][0] == board[row][1] == board[row][2] == symbol_1):
             winner = False
-            print("Player " + symbol_1 + ", you won!")
+            announce_win(symbol_1)
    
         elif (board[row][0] == board[row][1] == board[row][2] == symbol_2):
             winner = False
-            print("Player " + symbol_2 + ", you won!")
+            announce_win(symbol_2)
             
             
     # Check the columns
     for col in range (0, 3):
         if (board[0][col] == board[1][col] == board[2][col] == symbol_1):
             winner = False
-            print("Player " + symbol_1 + ", you won!")
+            announce_win(symbol_1)
         elif (board[0][col] == board[1][col] == board[2][col] == symbol_2):
             winner = False
-            print("Player " + symbol_2 + ", you won!")
+            announce_win(symbol_2)
 
     # Check the diagonals
-    if board[0][0] == board[1][1] == board[2][2] == symbol_1:
+    if board[0][0] == board[1][1] == board[2][2] == symbol_1 or board[0][2] == board[1][1] == board[2][0] == symbol_1:
         winner = False 
-        print("Player " + symbol_1 + ", you won!")
+        announce_win(symbol_1)
 
-    elif board[0][0] == board[1][1] == board[2][2] == symbol_2:
+    elif board[0][0] == board[1][1] == board[2][2] == symbol_2 or board[0][2] == board[1][1] == board[2][0] == symbol_2:
         winner = False
-        print("Player " + symbol_2 + ", you won!")
-
-    elif board[0][2] == board[1][1] == board[2][0] == symbol_1:
-        winner = False
-        print("Player " + symbol_1 + ", you won!")
-
-    elif board[0][2] == board[1][1] == board[2][0] == symbol_2:
-        winner = False
-        print("Player " + symbol_2 + ", you won!")
+        announce_win(symbol_2)
 
     return winner
     
 
 
-def illegal(board, symbol_1, symbol_2, row, column):
+def illegal():
     print("The square you picked is already filled. Pick another one.")
 
     
@@ -183,11 +177,11 @@ def report(count, winner, symbol_1, symbol_2):
 
 def main():
 # The main function
-    introduction = intro()
+    intro()
     board = create_grid()
-    pretty = printPretty(board)
+    print_pretty(board)
     symbol_1, symbol_2 = sym()
-    full = isFull(board, symbol_1, symbol_2) # The function that starts the game is also in here.
+    is_full(board, symbol_1, symbol_2) # The function that starts the game is also in here.
   
 
 
